944a014c2ecceeb5e770b88848e83f70cad4af7e
AyimWilliam <odhisayim@gmail.com>
2023-05-27 09:06:17 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    fibo_list = []
+
+    # Handling 0 & 1 cases
+    if length >= 1:
+        fibo_list.append(0)
+    if length >= 2:
+        fibo_list.append(1)
+
+    # Generating the sequence
+    for i in range(2, length):
+        fibo_list.append(fibo_list[i-1] + fibo_list[i-2])
+
+    # Printing the sequence
+    return fibo_list
+
+answer = print_fibonacci(10)
+print(answer)
